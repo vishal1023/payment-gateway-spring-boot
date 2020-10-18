0ef32be64849f2cbb9309c8a0b6cc99c370e9b20
Vishal <vakkalkote@gmail.com>
2020-10-18 13:07:33 +0530

    Initial commit to create maven project

---
+++ src/main/java/com/example/paymentgatewayspringboot/PaymentGatewaySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentgatewayspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewaySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewaySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paymentgatewayspringboot/PaymentGatewaySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentgatewayspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewaySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
